26a74ba622edbe5cb195e27c8097f84b38f56695
yosepan <tpq2tpq2@naver.com>
2023-10-04 16:51:38 +0900

    test6

--- app/src/main/java/com/example/myapplication/HomeCamActivity.java
+++ app/src/main/java/com/example/myapplication/HomeCamActivity.java
@@ -1,6 +1,44 @@
 package com.example.myapplication;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeCamActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeCamActivity extends AppCompatActivity {
+    private Button Move, Ctv;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Move = findViewById(R.id.move_btn);
+        Ctv = findViewById(R.id.cctv_btn);
+
+
+        Move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        HomeCamActivity.this, MoveActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Ctv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        HomeCamActivity.this, StreamingActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+    }
 }
